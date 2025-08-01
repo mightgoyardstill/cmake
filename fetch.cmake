4a44e52b4916cb9355d394639564a4721220c0fd
mightgoyardstill <mightgoyardstill@gmail.com>
2025-08-01 18:56:02 +0100

    Updated fetch.cmake

--- fetch.cmake
+++ fetch.cmake
@@ -43,15 +43,3 @@ function(fetch name url tag)
         endif()
     endif()
 endfunction()
-
-set (platform_libs)
-if (APPLE)
- list (APPEND platform_libs
-   "-framework WebKit \
-   -framework CoreServices \
-   -framework CoreAudio \
-   -framework CoreMIDI \
-   -framework Cocoa \
-   -framework CoreFoundation \
-   -framework CoreGraphics")
-endif (APPLE)
